27a53d9d7b612940ff0d3c220849e0c5a36b69e4
Magibalan S <143396923+magi2004@users.noreply.github.com>
2025-04-16 10:04:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Projects from './components/Projects'
+import Contacts from './components/Contacts'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <About />
+      <Services />
+      <Projects />
+      <Contacts />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Projects.jsx
@@ -0,0 +1,49 @@
+import ProjectImage from '../assets/logo.jpg'
+
+const projects = [
+    {
+        title: 'Portfolio',
+        technology: 'MERN Stack, Tailwind CSS',
+        description: 'This is a project description',
+        image: ProjectImage,
+        link: 'https://github.com/magi2004/Portfolio2'
+    },
+    {
+        title: 'Canteen management system',
+        technology: 'MERN Stack, Tailwind CSS',
+        description: 'This is a project description',
+        image: ProjectImage,
+        link:''
+    },
+    {
+        title: 'TBA',
+        technology: 'MERN Stack, Tailwind CSS',
+        description: 'This is a project description',
+        image: ProjectImage,
+        link:''
+    }
+]
+
+const Projects = () => {
+  return (
+    <div className='bg-gradient-to-br from-blue-400 to-cyan-500 text-white py-16' id='projects'>
+        <div className='container space-y-5 mx-auto px-8 md:px-16 lg:px-24'>
+            <h2 className='text-4xl text-center font-bold mb-12 bg-gradient-to-tr from-blue-700 to-cyan-500 rounded-full py-3'>Projects</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            {projects.map(projects => (
+                <div key={projects.id} className='bg-gradient-to-tr from-blue-700 to-cyan-500 p-8 rounded-xl transform transition-transform duration-100 hover:scale-110'>
+                    <h3 className='text-2xl font-bold mb-4'>{projects.title}</h3>
+                    <p>{projects.description}</p>
+                    <p>Technologies used: {projects.technology}</p>
+                    <img src={projects.image} alt='' className='w-48 h-48 rounded-3xl m-3 object-cover'/>
+                    <a href={projects.link} className='inline-block px-4 py-2 rounded-full bg-gradient-to-bl from-blue-700 to-cyan-500 text-orange-400 font-bold hover:text-white'>Github</a>
+                </div>
+
+            ))}
+            </div>
+            </div>
+        </div>
+  )
+}
+
+export default Projects
